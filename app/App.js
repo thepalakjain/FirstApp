c26075aae0ba55e8d55828f7ca92dc074ac84b8d
thepalakjain <palak.cis96@gmail.com>
2019-02-14 11:16:17 +0530

    adding folders

--- app/App.js
+++ app/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#F5FCFF"}}>
-        <Text>Hello world!</Text>
+        <Text>Hello oo world!</Text>
+        <Button title="Button!" color="#F51C11" />
       </View>
     );
   }
